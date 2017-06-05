ead0a42cbf08ed5e4ce4ccd196cac9dc613f6f8e
Gen Edwards <gen@fakepanda.com>
2017-06-05 07:15:59 -0400

    move index.html to root

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/index.html',
+  template: './index.html',
   filename: 'index.html',
   inject: 'body'
 });
